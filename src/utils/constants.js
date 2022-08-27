8a53d37b77c2016579a5906c5cdc987901f74ea4
yejine2 <yejine2@naver.com>
2022-08-27 15:17:30 +0900

    feat: 더미 데이터 추가

---
+++ src/utils/constants.js
@@ -0,0 +1,32 @@
+const DUMMY_DATA = [
+  {
+    id: "1",
+    title: "직장인 대출",
+    institution: "우리은행",
+    amount: "5,000,000",
+    categories: "직장인",
+  },
+  {
+    id: "2",
+    title: "학자금 대출",
+    institution: "한국장학재단",
+    amount: "2,500,000",
+    categories: "학생",
+  },
+  {
+    id: "3",
+    title: "대출",
+    institution: "정부24",
+    amount: "1,000,000",
+    categories: "전체",
+  },
+  {
+    id: "4",
+    title: "소상공인 대출",
+    institution: "정부24",
+    amount: "5,000,000",
+    categories: "자영업자",
+  },
+];
+
+export { DUMMY_DATA };
