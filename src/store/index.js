047ffaf9010d5aeecc55feea21443c6fac184bfb
Hwa-J <hj1103.code@gmail.com>
2022-08-31 14:49:32 +0900

    feat: curation store 생성 - #13

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import allProductSlice from "./slices/all-product-slice";
 import searchedProductSlice from "./slices/searched-product-slice";
+import { curationReducer } from "./slices/curation-product-slice";
 
 const store = configureStore({
   reducer: {
     allProduct: allProductSlice.reducer,
     searchedProduct: searchedProductSlice.reducer,
+    curation: curationReducer
   },
 });
 
