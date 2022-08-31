82b7a5f8ced5f93dcccf9025aceb078ba4353ab2
yejine2 <yejine2@naver.com>
2022-08-31 20:23:27 +0900

    feat: Cart component & reducer 구현 - #15

--- src/components/UI/Card/index.jsx
+++ src/components/UI/Card/index.jsx
@@ -1,12 +1,30 @@
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../../store/slices/cart-slice";
 import * as S from "./style";
-import { IoMdAddCircle } from "react-icons/io";
+import { IoMdAddCircle, IoMdRemoveCircle } from "react-icons/io";
+import { useLocation } from "react-router";
 
-function Card({ product }) {
+function Card({ product, onRemove }) {
+  const dispatch = useDispatch();
+  const location = useLocation().pathname;
+  const CART_URL = "/cart";
+
+  const cartItemAddHandler = (product) => {
+    dispatch(addToCart({ ...product, cartQuantity: 1 }));
+  };
+
   return (
     <S.CardContainer value={product.category}>
-      <S.CardCartBtn>
-        <IoMdAddCircle size="1.2rem" color="#6B23E0" />
-      </S.CardCartBtn>
+      {location !== CART_URL ? (
+        <S.CardCartBtn onClick={() => cartItemAddHandler(product)}>
+          <IoMdAddCircle size="1.2rem" color="#6B23E0" />
+        </S.CardCartBtn>
+      ) : (
+        <S.CardCartBtn onClick={onRemove}>
+          <IoMdRemoveCircle size="1.2rem" color="#6B23E0" />
+        </S.CardCartBtn>
+      )}
+
       <S.CardMainTitle>{product.title}</S.CardMainTitle>
       <S.CardAmountText>{`${product.amount} 원`}</S.CardAmountText>
       <S.CardInfoText>{product.institution}</S.CardInfoText>
