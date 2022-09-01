34d2b5d4ef233a86a4e66898e8001ed924018352
boksooni <8545652@naver.com>
2022-09-02 00:42:50 +0900

    feat: nav

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import {GiWallet, GiShoppingBag} from "react-icons/gi";
+import {BsCreditCardFill} from "react-icons/bs";
+import * as S from './style';
+import { useNavigate } from 'react-router-dom'
 
 function index() {
-  return <div>index</div>;
+  const test = () => {
+    console.log("zdfsa")
+  }
+
+  const navigate = useNavigate();
+
+  return <S.NavContainer>
+    <div>
+      <S.NavButton onClick={() => navigate("/curation")}>
+        <S.NavIcon><GiWallet size={45}/></S.NavIcon>
+        맞춤추천
+      </S.NavButton>
+    </div>
+    <div>
+      <S.NavButton onClick={() => navigate("/allproduct")}>
+        <S.NavIcon><GiShoppingBag size={45}/></S.NavIcon>
+        전체상품
+      </S.NavButton>
+    </div>
+    <div>
+      <S.NavButton onClick={() => navigate("/promotion")}>
+        <S.NavIcon><BsCreditCardFill size={45}/></S.NavIcon>
+        프로모션
+      </S.NavButton>
+    </div>
+  </S.NavContainer>;
 }
 
 export default index;
