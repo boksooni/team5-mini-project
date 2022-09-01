79aee7ea4fb3437eb99f7d68b2a63b4fae7e6368
boksooni <8545652@naver.com>
2022-09-02 00:43:32 +0900

    design: nav

---
+++ src/components/NavBar/style.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const NavContainer = styled.div`
+    width: 100%;
+    height: 104px;
+    background-color: #FFF;
+    display: inline-flex;
+    align-items: center;
+    justify-content: space-around;
+    border-radius: 20px 20px 0 0;
+    position: relative;
+
+`
+export const NavButton = styled.div`
+    font-size: small;
+    cursor: pointer;
+    align-items: stretch;
+    flex-direction: column;
+    display: flex;
+    color: green;
+
+`
+export const NavIcon = styled.div`
+    color: green;
+`
