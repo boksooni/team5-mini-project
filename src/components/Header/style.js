486a43b158cc5633b26968272f30288b286a9ef6
boksooni <8545652@naver.com>
2022-09-02 00:34:02 +0900

    design: header -2

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,30 +1,32 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.div`
-  width: 100%;
-  height: 52px;
-  display: flex;
-  margin: 0 auto;
-  position: relative;
+    width: 100%;
+    height: 52px;
+    display: flex;
+    margin: 0 auto;
+    position: relative;
 `;
 export const HeaderWrap = styled.div`
-  display: flex;
-  font-size: 30px;
+    display: flex;
+    font-size: 25px;
 `;
 export const HeaderLogo = styled.div`
-  cursor: pointer;
-  margin-left: 1rem;
+    color: #000;
+    cursor: pointer;
+    margin-left: 1rem;
 `;
 export const HeaderUl = styled.div`
-  display: flex;
-  list-style: none;
-  position: absolute;
-  right: 0px;
-  margin: 5px;
-  padding: 5px;
-  cursor: pointer;
-  align-items: center;
+    color: #000;
+    display: flex;
+    list-style: none;
+    position: absolute;
+    right: 0px;
+    margin: 5px;
+    padding: 5px;
+    cursor: pointer;
+    align-items: center;
 `;
 export const HeaderLi = styled.div`
-  margin-right: 1rem;
+    margin-right: 1rem;
 `;
