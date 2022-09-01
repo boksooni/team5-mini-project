b292378dd9b5ef71851abcdf055ef75abf95986b
yejine2 <yejine2@naver.com>
2022-09-01 13:41:01 +0900

    feat: 공통 버튼 컴포넌트 구현- #19

--- src/components/UI/Button/index.jsx
+++ src/components/UI/Button/index.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import styled, { css } from "styled-components";
+import theme from "../../../styles/theme";
 
-function index() {
-  return <div>index</div>;
-}
+const mainColor = theme.palette.purple;
 
-export default index;
+// 메인컬러 버튼
+const StyledButton = styled.button`
+  border: none;
+  border-radius: 1rem;
+  font-size: 0.8rem;
+  padding: 0.25rem 0.6rem;
+  margin: 0.2rem;
+  color: #fff;
+  outline: none;
+  cursor: pointer;
+  background: ${mainColor};
+
+  // 가로 100% 버튼
+  ${(props) =>
+    props.fullWidth &&
+    css`
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 86px;
+      background: ${mainColor};
+      border-radius: 10px 10px 0px 0px;
+      color: #fff;
+      font-size: 1.6rem;
+    `}
+`;
+
+const Button = (props) => <StyledButton {...props} />;
+
+export default Button;
