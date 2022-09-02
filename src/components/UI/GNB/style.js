ac839a7e1ebef447dc0a7a6bf25eae2331dd3a09
boksooni <8545652@naver.com>
2022-09-02 15:14:05 +0900

    design: gnb style - #26

---
+++ src/components/UI/GNB/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const GnbContainer = styled.div`
+    width: 100%;
+    height: 844px;
+
+`
+export const EditWrap = styled.div`
+    margin-top: 20px;
+    padding: 10px 0 10px 20px;
+    border-width: 1px 0 1px 0;
+    border-style: solid;
+    text-align: left;
+    
+    font-size: 20px;
+    cursor: pointer;
+`
+export const LogoutWrapper = styled.div`
+    text-align: center;
+    font-size: 20px;
+`
